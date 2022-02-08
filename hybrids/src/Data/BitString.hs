8c4feb9e085578e3cdb13594433eca54251d93c2
Nes Cohen <nes.cohen@group6.io>
2022-02-08 00:38:17 -0800

    implement evaluation for expressions

--- hybrids/src/Data/BitString.hs
+++ hybrids/src/Data/BitString.hs
@@ -13,12 +13,16 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE IncoherentInstances #-}
 
 module Data.BitString
     ( Bit(..)
     , BitString(..)
+    , FromBits(..)
     , xor
+    , append
     , enc8
+    , bitSingle
     ) where
 
 import Control.Applicative
@@ -47,17 +51,37 @@ newtype BitString n = BitString {unBitString :: Vec n Bit }
     deriving(Eq)
 instance Show (BitString N0) where
     show _ = ""
-instance Show (BitString ('S n)) where
-    show (BitString ((I bit) :* string)) =
-        show bit ++ case string of
-                      ØV     -> ""
-                      b :* s -> show (BitString $ b :* s)
+instance Show (BitString n) => Show (BitString ('S n)) where
+    show (BitString ((I bit) :* bs)) =
+        show bit ++ show (BitString bs)
 
 nat8 :: Nat N8
 nat8 = (S_ (S_ (S_ (S_ (S_ (S_ (S_ (S_ Z_))))))))
 
 xor :: BitString n -> BitString n -> BitString n
 xor (BitString s1) (BitString s2) = BitString $ vap (liftA2 bitXor) s1 s2
+
+append :: BitString n -> BitString m -> BitString (n + m)
+append (BitString s1) (BitString s2) = BitString (s1 .++ s2)
+
+bitSingle :: Bit -> BitString N1
+bitSingle b = BitString ((I b) :* ØV)
+
+class FromBits bs where
+    fromBits :: [Bit] -> Maybe bs
+
+instance FromBits (BitString N0) where
+    fromBits []     = Just $ BitString ØV
+    fromBits (b:bs) = Nothing
+
+instance FromBits (BitString n) => FromBits (BitString ('S n)) where
+    fromBits []     = Nothing
+    fromBits (b:bs) = do
+        rest <- fromBits bs
+        return $ append (bitSingle b) rest
+
+instance FromBits (BitString (n :: N)) where
+    fromBits = fromBits
 
 enc8 :: Int -> BitString N8
 enc8 n = BitString $ vgen nat8 b
--- hybrids/src/Hybrids.hs
+++ hybrids/src/Hybrids.hs
@@ -13,26 +13,77 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Hybrids where
 
-import Data.BitString
-import Data.Type.Fin
-import Type.Family.Nat
+import           Data.BitString
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           Data.Proxy
+import           Data.Type.Fin
+import           Type.Family.Nat
 
-data HVar n = String
+type HName = String
+data HVar (size :: N) = HVar HName
 
-data HExpr n where
+data HExpr (size :: N) where
     Variable    :: HVar n -> HExpr n
-    Literal     :: HExpr n -> HExpr n
+    Literal     :: BitString n -> HExpr n
     Xor         :: HExpr n -> HExpr n -> HExpr n
     Append      :: HExpr a -> HExpr b -> HExpr (a + b)
+    -- Call        :: HVar m -> [(forall (n :: N). (HVar n, HExpr n))] -> HExpr m
 
-data HTerm where
-    Assignment  :: HVar n -> HExpr n -> HTerm
-    Gets        :: HVar n -> Fin n   -> HTerm
+data HTerm (size :: N) where
+    Assign   :: HVar n -> HExpr n -> HTerm N0
+    Gets     :: HVar n -> HTerm N0
+    (:>)     :: HTerm n -> HTerm m -> HTerm m
+    Return   :: HExpr n -> HTerm n
+    -- Routine  :: HVar n -> [(forall (n :: N). HVar n)] -> HTerm n -> HTerm _Z
 
-main :: IO ()
-main = do
+type HVarContext = Map HName (Maybe [Bit])
+-- type HFnContext = forall (size :: N). Proxy size -> HName -> Maybe (HTerm size)
+
+data HContext = 
+     HContext { ctxVariables :: HVarContext
+              -- , ctxFunctions :: HFnContext
+              }
+
+type Error = String
+
+hVarLookup :: HContext -> HName -> Either Error (BitString n)
+hVarLookup ctx name =
+    case M.lookup name $ ctxVariables ctx of
+        Just (Just bs) -> case fromBits bs of
+                     Just str -> Right str
+                     Nothing  -> Left ("Error: Variable size mismatch")
+        _ -> Left ("Error: " <> name <> " used before defintion")
+
+otp8_test :: HTerm N8
+otp8_test = Gets (HVar "k")
+         :> Assign (HVar "m") (Literal $ enc8 100)
+         :> Return (Xor (Variable $ HVar "k") (Variable $ HVar "m"))
+
+evalHExpr :: HContext -> HExpr n -> Either Error (BitString n)
+evalHExpr ctx (Variable (HVar vName)) =
+    hVarLookup ctx vName
+evalHExpr ctx (Literal bs)   =
+    Right bs
+evalHExpr ctx (Xor e1 e2)    = do
+    b1 <- evalHExpr ctx e1
+    b2 <- evalHExpr ctx e2
+    return (b1 `xor` b2)
+evalHExpr ctx (Append e1 e2) = do
+    b1 <- evalHExpr ctx e1
+    b2 <- evalHExpr ctx e2
+    return (b1 `append` b2)
+-- evalHExpr ctx (Call fn args) = undefined
+
+evalHTerm :: HContext -> HTerm n -> Either Error (HContext, HTerm m)
+evalHTerm = undefined
+
+test :: IO ()
+test = do
     let ones = enc8 255
     putStrLn (show ones)
